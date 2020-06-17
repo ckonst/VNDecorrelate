742c2df999cae5d0cac164b9efb228adff661fd4
ckonst <christian.konstantinov98@gmail.com>
2020-06-17 02:14:10 -0400

    finished logarithmic impulse distribution

--- VNDecorrelate/vnd.py
+++ VNDecorrelate/vnd.py
@@ -1,22 +1,6 @@
 import random
-import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
 import numpy as np
-from functools import wraps
-from time import time
-
-#TODO: Logorthimic impulse distribution
-
-def measure(func):
-    @wraps(func)
-    def _time_it(*args, **kwargs):
-        start = int(round(time() * 1000))
-        try:
-            return func(*args, **kwargs)
-        finally:
-            end_ = int(round(time() * 1000)) - start
-            print(f"Total execution time: {end_ if end_ > 0 else 0} ms")
-    return _time_it
 
 def tofloat32(input_sig):
     '''
@@ -25,12 +9,12 @@ def tofloat32(input_sig):
     Parameters
     ----------
     input_sig : n-dimension numpy array
-        the input to convert to a 32 bit floating point array.
+        The input to convert to a 32 bit floating point array.
 
     Returns
     -------
     input_sig : n-dimension numpy array
-        the signal converted to 32 bit floating point array.
+        The signal converted to 32 bit floating point array.
     '''
     input_sig = input_sig.astype(np.float32, copy=False)
     input_sig = normalize(input_sig)
@@ -39,7 +23,7 @@ def tofloat32(input_sig):
 def restore(channel, original):
     '''
     Restores a channel of audio to the original level of the input
-    after a convolution
+    after a convolution.
 
     Parameters
     ----------
@@ -59,21 +43,21 @@ def restore(channel, original):
 
 def normalize(channel, min=-1, max=1):
     '''
-    Takes in an input channel and normalizes it to the min and max values
+    Takes in an input channel and normalizes it to the min and max values.
     
     Parameters
     ----------
     channel : numpy array
-        A single channel of the signal to normalize
+        A single channel of the signal to normalize.
     min : int, optional
-        the minimum value to set the channel to. The default is -1.
+        The minimum value to set the channel to. The default is -1.
     max : int, optional
-        the maximum value to set the channel to. The default is 1.
+        The maximum value to set the channel to. The default is 1.
 
     Returns
     -------
     channel : numpy array
-        The normalized channel
+        The normalized channel.
 
     '''
     #normalize to -1 to 1 first
@@ -85,25 +69,23 @@ def normalize(channel, min=-1, max=1):
         channel = pos + neg
     return channel
 
-@measure
 def vn_convolve(input_sig, impulse_response):
     '''
-    performs a segmented convolution
-    of the velvet noise sequence with a signal
-    we take advantage of the sparse nature of the sequence
-    to do convolution very quickly
+    Performs a convolution of the velvet noise sequence with a signal.
+    We take advantage of the sparse nature of the sequence
+    to do convolution very quickly.
     
     Parameters
     ----------
-    input_sig : numpy array
-        the input signal to convolve with the VNS
+    input_sig : n-dimension numpy array
+        The input signal to convolve with the generated VNS.
     impulse_response : 2d numpy array
-        a list containing the velvet noise sequences 
-        to convolve with the input signal for each channel
+        A list containing the velvet noise sequences 
+        to convolve with the input signal for each channel.
     
     Returns
     ----------
-    output_sig : numpy array
+    output_sig : 2d numpy array
          the output signal in stereo
     '''
     # if the input is mono, then duplicate it to stereo before convolving
@@ -138,83 +120,97 @@ def vn_convolve(input_sig, impulse_response):
         output_sig[:,i] = restore(output_sig[:,i], input_sig[:,i])
     return output_sig
 
-@measure
 def vn_generate(p, fs, dur):
     '''
-    generates a velvet noise sequence
-    with segmented decay so that the transients
-    are not smeared
-    
+    Generates a velvet noise sequence for a finite impulse response
+    to convolve with an input signal.
+    To avoid audible smearing of transients the following are applied:
+        - A segmented decay envelope.
+        - Logarithmic impulse distribution.
+    Segmented decay is preferred to exponential decay because it uses
+    less multiplication, and produces satisfactory results.
+
     Parameters
     ----------
     p : int
-        density measured in spikes/impulses per second 
+        The density measured in spikes/impulses per second.
     fs : int
-        sample rate
+        The sample rate.
     dur : float
-        duration of the sequence in seconds
+        The duration of the sequence in seconds.
     
     Returns
     ----------
     impulse_response: numpy array
-        the generated velvet noise sequence
+        The generated velvet noise sequence.
     
     '''
     # size of the sequence in samples
     Ls = int(fs * dur) 
-    # calculate the average spacing between two impulses (grid size)
+    # average spacing between two impulses (grid size)
     Td = fs / p
+    # total number of impulses
+    M = Ls / Td
+    # calculate the grid size between each impulse logarithmically
+    # use a lambda function to be able to apply this for each impulse index
+    Tdl = lambda m : (Ls / 100) * pow(10, (m - 2) / M)
     # number of segments
     I = 4
-    # coefficient values to applay segmented decay
+    # coefficient values to for segmented decay can be set manually
     si = [0.95, 0.5, 0.25, 0.1]
     impulse_response = np.zeros((Ls,))
-    for j, v in enumerate(impulse_response):
+    for i, v in enumerate(impulse_response):
         # first, randomize sign of the impulse
-        v = (2 * round(random.random()) - 1)
+        v = (2 * round(random.random()) - 1) 
         # now randomize the distance between impulses
-        index = round(j * Td + random.random() * (Td - 1))
+        index = round(i * Td + random.random() * (Td - 1))
         if index < len(impulse_response):
-            impulse_response[index] = v * si[int(index / (Ls / I))]
+            # multiply a segmented decay constant based on impulse index
+            v *= si[int(index / (Ls / I))]
+            # logarithmically distribute the impulses
+            index = round(random.random() * (Tdl(i) - 1)) + round(sum(Tdl(j) for j in range(i)))
+            impulse_response[index] = v
     return impulse_response
 
 #%%
-DENSITY = 1000  # measured in spikes/impulses per second 
-VNS_DURATION = 0.03 # duration of VNS in seconds
-LR_DELAY = 0.02
-sample_rate, sig_float32 = wavfile.read("drums.wav")
-vns = [vn_generate(DENSITY, sample_rate, VNS_DURATION), vn_generate(DENSITY, sample_rate, VNS_DURATION)]
-result = vn_convolve(sig_float32, vns)
-wavfile.write("drums_decorrelated.wav", sample_rate, result)
-
-sr, sf32 = wavfile.read("correct.wav")
-
-plt.figure()
-plt.plot(vns[0])
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.title('FIR')
-
-plt.figure()
-plt.plot(vns[1])
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.title('FIR')
-
-plt.figure()
-plt.plot(sig_float32)
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.title('Input')
-
-plt.figure()
-plt.plot(result)
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.title('Output')
-
-plt.figure()
-plt.plot(sf32)
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.title('Correct Solution')
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    DENSITY = 1000 # measured in spikes/impulses per second 
+    VNS_DURATION = 0.03 # duration of VNS in seconds
+    LR_DELAY = 0.02
+    sample_rate, sig_float32 = wavfile.read("drums.wav")
+    vns = [vn_generate(DENSITY, sample_rate, VNS_DURATION), vn_generate(DENSITY, sample_rate, VNS_DURATION)]
+    result = vn_convolve(sig_float32, vns)
+    wavfile.write("drums_decorrelated.wav", sample_rate, result)
+    
+    sr, sf32 = wavfile.read("correct.wav")
+    
+    plt.figure()
+    plt.plot(vns[0])
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.title('Velvet Noise Sequence L')
+    
+    plt.figure()
+    plt.plot(vns[1])
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.title('Velvet Noise Sequence R')
+    
+    plt.figure()
+    plt.plot(sig_float32)
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.title('Input')
+    
+    plt.figure()
+    plt.plot(result)
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.title('Output')
+    
+    plt.figure()
+    plt.plot(sf32)
+    plt.xlabel('Samples')
+    plt.ylabel('Amplitude')
+    plt.title('Correct Solution')
