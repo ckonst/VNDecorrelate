fix: rms normalization for stereo signals, feat: add stereo and dual mono normalization modes, add tests

VNDecorrelate/utils/dsp.py
@@ -1,7 +1,14 @@
+from enum import StrEnum
+
 import numpy as np
 from numpy.typing import NDArray
 
 EPSILON = 1e-10
+
+
+class NormalizeMode(StrEnum):
+    STEREO = 'stereo'
+    DUAL_MONO = 'dual_mono'
 
 
 def apply_stereo_width(input_sig: NDArray, width: float) -> NDArray:
@@ -66,17 +73,44 @@ def to_float32(input_sig: NDArray) -> NDArray[np.float32]:
     return input_sig.astype(np.float32, copy=False)
 
 
-def peak_normalize(input_sig: NDArray, epsilon: float = EPSILON) -> None:
+def peak_normalize(
+    input_sig: NDArray,
+    mode: NormalizeMode = NormalizeMode.DUAL_MONO,
+    epsilon: float = EPSILON,
+) -> None:
     """Normalize input_sig in-place to [-1, 1] using the calculated peaks."""
-    input_sig *= 1.0 / (np.max(np.abs(input_sig)) + epsilon)
+
+    match (input_sig.ndim, mode):
+        case (1, _) | (_, NormalizeMode.STEREO):
+            input_axis = None
+        case (_, NormalizeMode.DUAL_MONO):
+            input_axis = 0
+
+    input_sig *= 1.0 / (np.max(np.abs(input_sig), axis=input_axis) + epsilon)
 
 
 def rms_normalize(
-    input_sig: NDArray, output_sig: NDArray, epsilon: float = EPSILON
+    input_sig: NDArray,
+    output_sig: NDArray,
+    mode: NormalizeMode = NormalizeMode.DUAL_MONO,
+    epsilon: float = EPSILON,
 ) -> None:
     """Normalize output_sig in-place to the rms value of input_sig."""
-    output_sig *= np.sqrt(np.mean(np.square(input_sig))) / np.sqrt(
-        np.mean(np.square(np.sum(output_sig, axis=1))) + epsilon
+
+    match (input_sig.ndim, mode):
+        case (1, _) | (_, NormalizeMode.STEREO):
+            input_axis = None
+        case (_, NormalizeMode.DUAL_MONO):
+            input_axis = 0
+
+    match (output_sig.ndim, mode):
+        case (1, _) | (_, NormalizeMode.STEREO):
+            output_axis = None
+        case (_, NormalizeMode.DUAL_MONO):
+            output_axis = 0
+
+    output_sig *= np.sqrt(np.mean(np.square(input_sig), axis=input_axis)) / np.sqrt(
+        np.mean(np.square(output_sig), axis=output_axis) + epsilon
     )
 
 

tests/test_dsp.py
@@ -2,37 +2,49 @@ from unittest import TestCase
 
 import numpy as np
 
-from VNDecorrelate.utils import dsp
+from VNDecorrelate.utils.dsp import (
+    LR_to_MS,
+    MS_to_LR,
+    NormalizeMode,
+    apply_stereo_width,
+    cross_correlogram,
+    encode_signal_to_side_channel,
+    mono_to_stereo,
+    peak_normalize,
+    rms_normalize,
+    stereo_to_mono,
+    to_float32,
+)
 
 
 class DSPTestCase(TestCase):
     def test__apply_stereo_width(self):
         x1 = np.random.uniform(size=(100, 2))
-        y1 = dsp.apply_stereo_width(x1, 1.0)
-        self.assertAlmostEqual(np.sum(dsp.LR_to_MS(y1)[:, 0]), 0.0)
-        self.assertNotAlmostEqual(np.sum(dsp.LR_to_MS(y1)[:, 1]), 0.0)
+        y1 = apply_stereo_width(x1, 1.0)
+        self.assertAlmostEqual(np.sum(LR_to_MS(y1)[:, 0]), 0.0)
+        self.assertNotAlmostEqual(np.sum(LR_to_MS(y1)[:, 1]), 0.0)
         x2 = np.random.uniform(size=(100, 2))
-        y2 = dsp.apply_stereo_width(x2, 0.0)
-        self.assertAlmostEqual(np.sum(dsp.LR_to_MS(y2)[:, 1]), 0.0)
-        self.assertNotAlmostEqual(np.sum(dsp.LR_to_MS(y2)[:, 0]), 0.0)
+        y2 = apply_stereo_width(x2, 0.0)
+        self.assertAlmostEqual(np.sum(LR_to_MS(y2)[:, 1]), 0.0)
+        self.assertNotAlmostEqual(np.sum(LR_to_MS(y2)[:, 0]), 0.0)
         x3 = np.full((100, 2), 100)
-        y3 = dsp.apply_stereo_width(x3, 0.5)
+        y3 = apply_stereo_width(x3, 0.5)
         self.assertAlmostEqual(np.sum(y3), 100 * 100)
         x4 = np.full((100, 2), 100)
-        y4 = dsp.apply_stereo_width(x4, 0.25)
+        y4 = apply_stereo_width(x4, 0.25)
         self.assertTrue(not np.array_equal(x4, y4))
 
     def test__encode_signal_to_side_channel(self):
         x1 = np.array([])
         x2 = np.array([])
         with self.assertRaises(ValueError):
-            dsp.encode_signal_to_side_channel(x1, x2)
+            encode_signal_to_side_channel(x1, x2)
         x1 = np.zeros((100, 2))
         with self.assertRaises(ValueError):
-            dsp.encode_signal_to_side_channel(x1, x2)
+            encode_signal_to_side_channel(x1, x2)
         x2 = np.zeros((120, 2))
         with self.assertRaises(ValueError):
-            dsp.encode_signal_to_side_channel(x1, x2)
+            encode_signal_to_side_channel(x1, x2)
         x2 = np.column_stack(
             (
                 np.zeros(
@@ -41,108 +53,127 @@ class DSPTestCase(TestCase):
                 np.full((100,), 100),
             )
         )
-        y1 = dsp.encode_signal_to_side_channel(x1, x2)
-        y1 = dsp.LR_to_MS(y1)
+        y1 = encode_signal_to_side_channel(x1, x2)
+        y1 = LR_to_MS(y1)
         self.assertTrue(np.array_equal(y1[:, 1], (x2[:, 0] - x2[:, 1]) / 2))
         x2 = np.full((100, 2), 100)
-        y1 = dsp.encode_signal_to_side_channel(x1, x2)
-        y1 = dsp.LR_to_MS(y1)
+        y1 = encode_signal_to_side_channel(x1, x2)
+        y1 = LR_to_MS(y1)
         self.assertTrue(np.array_equal(y1[:, 1], np.zeros((100,))))
 
     def test_to_float32(self):
         x = np.array([1, 2, 3, 4], dtype=np.int32)
-        self.assertEqual(dsp.to_float32(x).dtype, np.float32)
+        self.assertEqual(to_float32(x).dtype, np.float32)
         x = np.array([1, 2, 3, 4], dtype=np.float32)
-        self.assertEqual(dsp.to_float32(x).dtype, np.float32)
-        self.assertTrue(dsp.to_float32(x) is x)
+        self.assertEqual(to_float32(x).dtype, np.float32)
+        self.assertTrue(to_float32(x) is x)
 
     def test_peak_normalize(self):
         x1 = np.random.normal(loc=0.0, scale=100.0, size=(100, 100))
         x2 = np.zeros((10_000))
         x3 = np.random.uniform(low=2.0, high=10.0, size=(50_000, 2))
         x4 = np.random.uniform(size=(100, 100))
-        dsp.peak_normalize(x1)
+
+        peak_normalize(x1)
         self.assertLessEqual(np.max(np.abs(x1)), 1.0)
-        dsp.peak_normalize(x2)
+        peak_normalize(x2)
         self.assertLessEqual(np.max(np.abs(x2)), 1.0)
-        dsp.peak_normalize(x3)
+        peak_normalize(x3)
         self.assertLessEqual(np.max(np.abs(x3)), 1.0)
-        dsp.peak_normalize(x4)
+        peak_normalize(x4)
         self.assertLessEqual(np.max(np.abs(x1)), 1.0)
 
+        x5 = np.array([0.707, 0.707, 0.707])
+        x6 = np.array([0.707, 0.707, 0.707])
+        peak_normalize(x5)
+        self.assertFalse(np.allclose(x5, x6))
+
+        x7 = np.array([[0.707, 0.3535], [0.707, 0.3535]])
+        x8 = np.array([[0.707, 0.3535], [0.707, 0.3535]])
+        peak_normalize(x7)
+        self.assertFalse(np.allclose(x7, x8))
+
     def test_rms_normalize(self):
-        x1 = np.random.normal(loc=0.0, scale=100.0, size=(100, 100))
-        x2 = np.zeros((10_000))
-        x3 = np.random.uniform(low=2.0, high=10.0, size=(50_000, 2))
-        x4 = np.random.uniform(size=(100, 100))
-        sum_ = np.sum(x4)
-        dsp.rms_normalize(x1, x4)
-        self.assertNotAlmostEqual(sum_, np.sum(x4))
-        dsp.rms_normalize(x2, x4)
-        self.assertNotAlmostEqual(sum_, np.sum(x4))
-        dsp.rms_normalize(x3, x4)
-        self.assertNotAlmostEqual(sum_, np.sum(x4))
+        x = np.array([0.707, 0.707, 0.707])
+        y = np.array([1.0, 1.0, 1.0])
+        rms_normalize(x, y)
+        self.assertTrue(np.allclose(x, y))
+
+        x = np.array([[0.707, 0.3535], [0.707, 0.3535]])
+        y = np.array([[1.0, 1.0], [1.0, 1.0]])
+        rms_normalize(x, y, mode=NormalizeMode.DUAL_MONO)
+        self.assertTrue(np.allclose(x, y))
+
+        x = np.array([[0.707, 0.3535], [0.707, 0.3535]])
+        y = np.array([[1.0, 1.0], [1.0, 1.0]])
+        rms_normalize(x, y, mode=NormalizeMode.STEREO)
+        self.assertTrue(np.allclose(np.array([0.55893258, 0.55893258]), y))
+
+        x = np.array([0.707, 0.707, 0.707])
+        y = np.array([[1.0, 1.0], [1.0, 1.0]])
+        rms_normalize(x, y, mode=NormalizeMode.STEREO)
+        self.assertAlmostEqual(x[0], y[0, 0])
 
     def test_mono_to_stereo(self):
         x = np.array([1, 2, 3, 4])
-        y = dsp.mono_to_stereo(x)
+        y = mono_to_stereo(x)
         y_truth = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([])
-        y = dsp.mono_to_stereo(x)
+        y = mono_to_stereo(x)
         y_truth = np.zeros((0, 2))
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
         with self.assertRaises(ValueError):
-            dsp.mono_to_stereo(x)
+            mono_to_stereo(x)
 
     def test_stereo_to_mono(self):
         x = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
-        y = dsp.stereo_to_mono(x)
+        y = stereo_to_mono(x)
         y_truth = np.array([1, 2, 3, 4])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.zeros((0, 2))
-        y = dsp.stereo_to_mono(x)
+        y = stereo_to_mono(x)
         y_truth = np.array([])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
         x = np.array([1, 2, 3, 4])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
 
     def test_LR_to_MS(self):
         x = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
-        y = dsp.LR_to_MS(x)
+        y = LR_to_MS(x)
         y_truth = np.array([[1, 0], [2, 0], [3, 0], [4, 0]])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([[1, 2], [2, 4], [3, 6], [4, 8]])
-        y = dsp.LR_to_MS(x)
+        y = LR_to_MS(x)
         y_truth = np.array([[1.5, -0.5], [3, -1], [4.5, -1.5], [6, -2]])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
         x = np.array([1, 2, 3, 4])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
 
     def test_MS_to_LR(self):
         x = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
-        y = dsp.MS_to_LR(x)
+        y = MS_to_LR(x)
         y_truth = np.array([[2, 0], [4, 0], [6, 0], [8, 0]])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([[1, 2], [2, 4], [3, 6], [4, 8]])
-        y = dsp.MS_to_LR(x)
+        y = MS_to_LR(x)
         y_truth = np.array([[3, -1], [6, -2], [9, -3], [12, -4]])
         self.assertTrue(np.array_equal(y, y_truth))
         x = np.array([])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
         x = np.array([1, 2, 3, 4])
         with self.assertRaises(ValueError):
-            dsp.stereo_to_mono(x)
+            stereo_to_mono(x)
 
     def test_cross_correlogram(self):
         fs = 16000  # 16 kHz sample rate
@@ -151,7 +182,7 @@ class DSPTestCase(TestCase):
         y = np.sin(2 * np.pi * 440 * t + np.pi / 6) + 0.05 * np.random.normal(
             size=len(t)
         )  # Phase shifted sin wave + small noise
-        ccg = dsp.cross_correlogram(
+        ccg = cross_correlogram(
             x,
             y,
             sample_rate_hz=fs,