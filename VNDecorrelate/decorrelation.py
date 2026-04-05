fix: segmented decay envelope bug fix: logarithmic impulse distribution off-by-one feat: exponential decay function sampling for segmented decay envelope

VNDecorrelate/decorrelation.py
@@ -8,6 +8,7 @@ import numpy as np
 from numpy.typing import NDArray
 
 from vndecorrelate.utils.dsp import (
+    IDENTITY_ENVELOPE,
     LR_to_MS,
     MS_to_LR,
     apply_log_distribution,
@@ -378,6 +379,9 @@ class VelvetNoise(Decorrelator):
                 f'Velvet Noise Filter of length {self.fir_length_samples} with {self.num_impulses} impulses is not sparse. ({density=:.2f})\n'
                 '\tnum_impulses must be less than half the FIR length in samples.'
             )
+        # If segment_envelope is empty, use a single segment with no decay.
+        if not self.segment_envelope:
+            self.segment_envelope = IDENTITY_ENVELOPE
         self._velvet_noise = self._generate()
 
     def convolve(self, input_signal: NDArray) -> NDArray:
@@ -395,7 +399,7 @@ class VelvetNoise(Decorrelator):
                             segment_buffer[: -impulse_index or sig_len],
                             operator,  # either __isub__ or __iadd__
                         )(input_signal[impulse_index:, channel_index])
-                if self.segment_envelope:
+                if self.segment_envelope != IDENTITY_ENVELOPE:
                     segment_buffer *= self.segment_envelope[segment_index]
                 output_signal[:, channel_index] += segment_buffer
                 segment_buffer.fill(0)
@@ -467,16 +471,18 @@ class VelvetNoise(Decorrelator):
         rng = np.random.default_rng(self.seed)
 
         velvet_noise = _ParallelVelvetNoise(fir_length_samples=self.fir_length_samples)
-        num_segments = max(
-            len(self.segment_envelope), 1
-        )  # If segment_envelope is empty, use a single segment with no decay.
+        num_segments = len(self.segment_envelope)
 
         log_distribution = generate_log_distribution(
             self.log_distribution_strength, self.num_impulses
         )
 
-        # number of samples between each logarithmically distributed impulse
-        log_impulse_intervals = np.cumsum(log_distribution)
+        # Calculate number of samples between each logarithmically distributed impulse:
+        # Start by taking the cumulative sum, then shift left by 1 if we have a log distribution strength of 0.0
+        # This is done to be consistent with `uniform_density` i.e. we fix an off-by-one error in the uniform distribution case.
+        log_impulse_intervals = np.cumsum(log_distribution) - (
+            1.0 if self.log_distribution_strength == 0.0 else 0.0
+        )
 
         log_impulse_intervals = log_impulse_intervals[:-1] * (
             self.fir_length_samples / log_impulse_intervals[-1]
@@ -554,8 +560,12 @@ def generate_velvet_noise(
         log_distribution_strength, num_impulses
     )
 
-    # number of samples between each logarithmically distributed impulse
-    log_impulse_intervals = np.cumsum(log_distribution)
+    # Calculate number of samples between each logarithmically distributed impulse:
+    # Start by taking the cumulative sum, then shift left by 1 if we have a log distribution strength of 0.0
+    # This is done to be consistent with `uniform_density` i.e. we fix an off-by-one error in the uniform distribution case.
+    log_impulse_intervals = np.cumsum(log_distribution) - (
+        1.0 if log_distribution_strength == 0.0 else 0.0
+    )
 
     log_impulse_intervals = log_impulse_intervals[:-1] * (
         fir_length_samples / log_impulse_intervals[-1]

VNDecorrelate/utils/dsp.py
@@ -3,7 +3,9 @@ from enum import StrEnum
 import numpy as np
 from numpy.typing import NDArray
 
-EPSILON = 1e-10
+EPSILON: float = 1e-10
+
+IDENTITY_ENVELOPE: tuple[float] = (1.0,)
 
 
 class NormalizeMode(StrEnum):
@@ -166,7 +168,15 @@ def MS_to_LR(input_signal: NDArray) -> None:
 
 
 def generate_log_distribution(strength: float, size: int) -> NDArray:
-    return (10.0 ** (2 * strength * (np.arange(size + 1) / size))) / 100.0
+    """Return an `NDArray` of size: `size + 1` containing exponentially increasing values."""
+    return (
+        (10.0 ** (2.0 * strength * (np.arange(size + 1.0) / size)))
+        /
+        # Applying the strength term to the denominator helps with the uniform density case
+        # where `strength = 0.0` by reducing the normalization constant to 1.0
+        # to generate a sequence that increases linearly, while still parameterized by strength.
+        (100.0 * ((1.0 + (strength * 99.0)) / 100.0))
+    )
 
 
 def apply_log_distribution(
@@ -175,7 +185,7 @@ def apply_log_distribution(
     log_impulse_intervals: NDArray,
 ) -> NDArray:
     """Return the randomized position of the impulse in the FIR, distributing logarithmically towards the start of the filter."""
-    return (np.round(randoms * (log_distribution - 1)) + log_impulse_intervals).astype(
+    return np.round(randoms * (log_distribution - 1) + log_impulse_intervals).astype(
         np.int32
     )
 
@@ -291,3 +301,32 @@ def polar_coordinates(
             r /= r_max
 
     return r, theta
+
+
+def exponential_decay(t: float, k: float = 2) -> float:
+    """evaluate :math:`e^{-kt}`"""
+    return np.e ** (-k * t)
+
+
+def generate_decay_envelope(
+    num_segments: int, segment_position: float
+) -> tuple[float, ...]:
+    """_summary_
+
+    Parameters
+    ----------
+    num_segments : int
+        The number of segments to generate.
+
+    segment_position : float
+        A percent [0.0, 1.0] to shift the intra-segment sample location to.
+
+    Returns
+    -------
+    tuple[float, ...]:
+        A tuple of `num_segments` floats that define the decay envelope.
+    """
+    return tuple(
+        exponential_decay((t / num_segments) + (segment_position * 1 / num_segments))
+        for t in range(num_segments)
+    )